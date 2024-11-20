f039291f34cb937ad3dcb288949ed1ed9dc3affd
Allofme2025 <yolattanavong789@gmail.com>
2024-11-20 09:05:40 +0700

    first commit

---
+++ server/config/connectDB.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
+
+if(!process.env.MONGODB_URI){
+    throw new Error(
+        "Please provide MONGODB_URI in the .env file"
+    )
+}
+
+async function connectDB(){
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,
+            {
+                dbName: "x6tech",
+              }
+        )
+        console.log("connect DB")
+    } catch (error) {
+        console.log("Mongodb connect error",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
